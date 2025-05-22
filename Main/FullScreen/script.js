d2d8eb6f86408aef4024e5e4f84027e1ea1f931f
drxg0nG <kaavishdeepan@gmail.com>
2025-05-22 18:56:42 +0000

    day 2 in class
    Co-authored-by: Amising2000 <Amising2000@users.noreply.github.com>

---
+++ Main/FullScreen/script.js
@@ -0,0 +1,22 @@
+let fullScreen = document.documentElement;
+
+/* View in fullscreen */
+function startFullscreen() {
+    if (fullScreen.requestFullscreen) {
+        fullScreen.requestFullscreen();
+    } else if (fullScreen.webkitRequestFullscreen) { /* Safari */
+        fullScreen.webkitRequestFullscreen();
+    } else if (fullScreen.msRequestFullscreen) { /* IE11 */
+        fullScreen.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
