cdde55fa5e902f75f57adf9e5b102cad05d24391
drxg0nG <kaavishdeepan@gmail.com>
2025-05-31 00:53:19 +0000

    5/30/25 pt2
    Co-authored-by: Amising2000 <Amising2000@users.noreply.github.com>

--- StartPage/script.js
+++ StartPage/script.js
@@ -1,6 +1,5 @@
 function startNewGame() {
     localStorage.setItem("escapeLevel", 1);
-    alert("New game started! (This is just the start page.)");
 }
 
 function continueGame() {
@@ -14,7 +13,6 @@ function continueGame() {
 
 
 // Matrix Background
-
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789{}[]$@!";
 const matrix = document.getElementById("matrix")
 
@@ -25,17 +23,28 @@ function createCodeStream() {
     // Where its going to fall from
     codeStream.style.transform = `translateX(${Math.random() * 100}vw)`
 
+    // Duration for the animation
+    codeStream.style.animationDuration = (4 + Math.random() * 4) + 's' // !!!!!!!!!!!!!!!!!!!!!!!!!!
+
     // Random font size
     const fontSize = 14 + Math.random() * 10
     codeStream.style.fontSize = fontSize + 'px'
 
     // Build code stream
     let codeStreamContent = ''
-    const codeStreamLength = 12 + Math.random() * 15
+    const codeStreamLength = 12 + Math.floor(Math.random() * 15)
+    for (let i = 0; i < codeStreamLength; i++) {
+        const char = letters.charAt(Math.floor(Math.random() * letters.length))
+        codeStreamContent += char + '<br>'
+    }
+    codeStream.innerHTML = codeStreamContent
 
+    matrix.appendChild(codeStream)
 
-
-
+    setTimeout(() => {
+      container.removeChild(codeStream);
+    }, (parseFloat(codeStream.style.animationDuration) * 1000))
 }
 
-
+// Create new stream
+setInterval(createCodeStream, 25)
